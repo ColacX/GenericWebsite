fdc742039767bb7d62718efd75950b28a82060c5
ColacX <DanielZheng1989@gmail.com>
2016-02-26 14:05:00 +0100

    added: empty migration

---
+++ DatabaseLayer/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using DatabaseLayer.Tables;
+using System.Data.Entity;
+
+namespace DatabaseLayer
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<Filedata> Filedata { get; set; }
+	}
+}
