4efbc5050ea89e010f522cba98bf0756292fccae
ColacX <DanielZheng1989@gmail.com>
2016-02-26 10:53:53 +0100

    reorganised

--- IISApp/Scripts/Site/Site.js
+++ IISApp/Scripts/Site/Site.js
@@ -1,5 +1,7 @@
 ﻿'use strict';
 
+console.log("Site.js");
+
 //define module and dependencies to other modules
 angular.module('SiteModule', ['ui.router']);
 
@@ -18,32 +20,35 @@ angular.module('SiteModule').directive('smTestDirective', ['$document', function
 
 //define ui-router view
 angular.module('SiteModule').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-	// For any unmatched url, redirect to /state1
-	$urlRouterProvider.otherwise("/state1");
-
-	// Now set up the states
+	
+	//define the states
 	$stateProvider
 		.state('state1', {
 			url: "/state1",
-			templateUrl: "Home/Test1",
 			views: {
-				"viewA": { templateUrl: "Home/Test1" }
+				"mainView": { templateUrl: "/Phone/Home/Test1" }
 			}
 		})
 		.state('state2', {
 			url: "/state2",
-			templateUrl: "Home/Test2",
 			views: {
-				"viewA": { templateUrl: "Home/Test2" }
+				"mainView": { templateUrl: "/Phone/Home/Test2" }
 			}
 		});
+	
+	//do not do redirects it gets too complicated
+	//define redirects
+	//$urlRouterProvider.when("", "/");
+	//$urlRouterProvider.otherwise("oops");
 }]);
+
+////preload templates into cache
+//angular.module('SiteModule').run(function ($templateCache) {
+//	$templateCache.put("Test2", 'This is the content of the template');
+//});
 
 //define controllers
 angular.module('SiteModule').controller('SiteController', ['$scope', '$compile', function ($scope, $compile) {
-	console.log("site controller")
 	var self = this;
-	var result = $compile("<a ui-sref='state1'>State 1</a>")($scope);
-
-	console.log(result);
 }]);
+
